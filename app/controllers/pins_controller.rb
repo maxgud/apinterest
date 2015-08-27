ef79b47519fbe88878ed8f8666bcf25708f26c16
maxgud <maxgud@gmail.com>
2015-08-27 16:52:13 -0600

    pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -7,7 +7,7 @@ class PinsController < ApplicationController
   respond_to :html
 
   def index
-    @pins = Pin.all.order("created_at DESC").limit(3)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
     respond_with(@pins)
   end
 
