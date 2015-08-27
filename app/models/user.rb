80662aeb713ab26fbad369e668af7c31be4c4deb
maxgud <maxgud@gmail.com>
2015-08-27 17:53:20 -0600

    usernames instead of emails on pins

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
+  validates :image, presence: true
+  validates :description, presence: true
+
 end
